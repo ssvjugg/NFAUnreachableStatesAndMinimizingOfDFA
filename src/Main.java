b0220bafcf3c6ec8e3467356bb0494558f53c6b0
usernamedrew <59914219+ssvjugg@users.noreply.github.com>
2024-12-10 22:16:55 +0300

    Added reading from a file

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.*;
+
+/*
+    - S 0|A 1|B
+    - A 0|E 1|B
+    - B 0|D 1|C
+    - C 0|A 1|B
+    - D 0|F 1|C
+    - E 0|J 1|F
+    - F 0|I 1|E
+    - J 0|G 1|F
+    - I 1|E
+    - G 0|G 1|G e|H
+    f H
+ */
 
 //UnreachableStates
 public class Main {
     public static void main(String[] args) {
-
+        try {
+            var l = State.readNkaFromFile(Paths.get("nka.txt"));
+            for (State s : l)
+                System.out.println(s);
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
--- src/State.java
+++ src/State.java
@@ -1,8 +1,13 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.*;
 
 public class State {
     private final String name;
-    private final boolean finalState;
+    private boolean finalState;
     private final Map<String, List<State>> transitions;
 
     public State(String name, boolean finalState) {
@@ -17,7 +22,7 @@ public class State {
      * @param v state that will be current after transition by symbol k
      */
     public void addTransition(String k, State v) {
-        
+        transitions.computeIfAbsent(k, key -> new ArrayList<>()).add(v);
     }
 
     public boolean isFinalState() {
@@ -26,5 +31,65 @@ public class State {
 
     public String getName() {
         return name;
+    }
+
+    /**
+     * This function read from states from file
+     * @param p Path to file in system
+     * @return List of States
+     */
+    public static List<State> readNkaFromFile(Path p) throws IOException {
+        List<State> states = new ArrayList<>();
+        try (BufferedReader br = Files.newBufferedReader(p, StandardCharsets.UTF_8)) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] ln = line.split("\\s+");
+                State curState;
+                if ((curState = containsState(ln[1], states)) == null) {
+                    curState = new State(ln[1], "f".equals(ln[0]));
+                    states.add(curState);
+                }
+                else if ("f".equals(ln[0])){
+                    curState.finalState = true;
+                }
+
+                for (int i = 2; i < ln.length; i++) {
+                    String[] transition = ln[i].split("\\|");
+                    State tempState;
+                    if ((tempState = containsState(transition[1], states)) == null) {
+                        tempState = new State(transition[1], false);
+                        curState.addTransition(transition[0], tempState);
+                        states.add(tempState);
+                    }
+                    else {
+                        curState.addTransition(transition[0], tempState);
+                    }
+                }
+            }
+        }
+        return states;
+    }
+
+    private static State containsState(String name, List<State> lst) {
+        for (State s : lst) {
+            if (name.equals(s.getName()))
+                return s;
+        }
+        return null;
+    }
+
+    public static void removeUnreachableStates(List<State> states) {
+
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(isFinalState() ? "f " : "- ").append(name);
+        for (var t : transitions.entrySet()) {
+            for (var s : t.getValue())
+                sb.append(String.format(" %s|%s", t.getKey(), s.name));
+        }
+        return sb.toString();
     }
 }
